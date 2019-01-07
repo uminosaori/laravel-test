860d341e6ea7bda0d5f4176f0618ac11b959d1a6
Saori <39708751+uminosaori@users.noreply.github.com>
2019-01-07 23:53:23 +0500

    dz1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome, Blade!
+                    Hello from Blade!
                 </div>
 
             
